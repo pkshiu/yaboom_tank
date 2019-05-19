d0aa7b3501524aab8b32bbd33c558125bc10cf46
PK Shiu <pk@pkshiu.com>
2019-05-19 21:22:19 +0100

    simple test code

---
+++ right_test.py
@@ -0,0 +1,70 @@
+import RPi.GPIO as GPIO
+import time
+
+#Definition of  motor pin 
+
+# IN1 is shorted???
+
+# A: 20, 21 = gpio 28, 29
+IN1 = 20
+IN2 = 21
+
+# B: 19, 26 = gpio 24, 25
+IN3 = 19
+IN4 = 26
+
+# pwm A 16 = gpio.27
+ENA = 16
+
+# pwm B 13 = gpio.23
+ENB = 13
+
+#Set the GPIO port to BCM encoding mode.
+GPIO.setmode(GPIO.BCM)
+
+#Ignore warning information
+GPIO.setwarnings(False)
+
+#Motor pin initialization operation
+def motor_init():
+    global pwm_ENA
+    global pwm_ENB
+    GPIO.setup(ENA,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN1,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN2,GPIO.OUT,initial=GPIO.LOW)
+
+    GPIO.setup(ENB,GPIO.OUT,initial=GPIO.HIGH)
+    GPIO.setup(IN3,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(IN4,GPIO.OUT,initial=GPIO.LOW)
+    #Set the PWM pin and frequency is 2000hz
+    pwm_ENA = GPIO.PWM(ENA, 2000)
+    pwm_ENB = GPIO.PWM(ENB, 2000)
+
+#advance
+def run():
+    # GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+
+    # GPIO.output(IN3, GPIO.LOW)
+    GPIO.output(IN3, GPIO.HIGH)
+    GPIO.output(IN4, GPIO.LOW)
+    # GPIO.output(IN3, GPIO.HIGH)
+    # GPIO.output(IN4, GPIO.LOW)
+    #PWM duty cycle is set to 100（0--100）
+    pwm_ENA.start(20)
+    pwm_ENB.start(20)
+
+#delay 2s
+time.sleep(2)
+
+#The try/except statement is used to detect errors in the try block.
+#the except statement catches the exception information and processes it.
+try:
+    motor_init()
+    while True:
+        run()
+except KeyboardInterrupt:
+    pass
+pwm_ENA.stop()
+pwm_ENB.stop()
+GPIO.cleanup()
---
+++ ultrasonic_test.py
@@ -0,0 +1,93 @@
+import RPi.GPIO as GPIO
+import time
+
+#Definition of  pin 
+IN1 = 20
+IN2 = 21
+IN3 = 19
+IN4 = 26
+ENA = 16
+ENB = 13
+
+#Definition of button
+key = 8
+
+#Definition of ultrasonic module pin
+EchoPin = 0
+TrigPin = 1
+
+#Definition of servo pin
+ServoPin = 23
+
+#Set the GPIO port to BCM encoding mode.
+GPIO.setmode(GPIO.BCM)
+
+#Ignore warning information
+GPIO.setwarnings(False)
+
+#Motor pins are initialized into output mode
+#Key pin is initialized into input mode
+#Ultrasonic pin initialization
+def init():
+    global pwm_servo
+    GPIO.setup(key,GPIO.IN)
+    GPIO.setup(ServoPin, GPIO.OUT)
+    GPIO.setup(EchoPin,GPIO.IN)
+    GPIO.setup(TrigPin,GPIO.OUT)
+    
+    pwm_servo = GPIO.PWM(ServoPin, 50)
+    pwm_servo.start(0)
+
+
+#Button detection
+def key_scan():
+    while GPIO.input(key):
+         pass
+    while not GPIO.input(key):
+         time.sleep(0.01)
+         if not GPIO.input(key):
+             time.sleep(0.01)
+         while not GPIO.input(key):
+             pass
+                
+#Ultrasonic function
+def Distance_test():
+    GPIO.output(TrigPin,GPIO.HIGH)
+    time.sleep(0.000015)
+    GPIO.output(TrigPin,GPIO.LOW)
+    while not GPIO.input(EchoPin):
+        pass
+    t1 = time.time()
+    while GPIO.input(EchoPin):
+        pass
+    t2 = time.time()
+    print("distance is %d " % (((t2 - t1)* 340 / 2) * 100))
+    time.sleep(0.01)
+    return ((t2 - t1)* 340 / 2) * 100
+    
+#The servo rotates to the specified angle
+def servo_appointed_detection(pos):
+    for i in range(18):
+        pwm_servo.ChangeDutyCycle(2.5 + 10 * pos/180)    
+
+
+time.sleep(2)
+
+#The try/except statement is used to detect errors in the try block.
+#the except statement catches the exception information and processes it.
+try:
+    init()
+    # key_scan()
+    servo_appointed_detection(0)
+    time.sleep(2)
+    servo_appointed_detection(90)
+    time.sleep(2)
+    servo_appointed_detection(160)
+    time.sleep(2)
+    servo_appointed_detection(90)
+    time.sleep(2)
+       
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
